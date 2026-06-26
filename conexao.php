788227afec705edcdfb990f2a8a2102a0a7d5a2d
Tomioka愛 <00001127021424sp@al.educacao.sp.gov.br>
2026-06-26 01:46:30 -0300

    Add files via upload

---
+++ conexao.php
@@ -0,0 +1,24 @@
+<?php
+
+    $dbHost = 'Localhost';
+    $dbUsername = 'root';
+    $dbPassword = '';
+    $dbName = 'gym_core';
+
+    $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
+
+//     if($conexao->connect_errno)
+
+// {
+//     echo "Erro";
+// }
+
+// else
+
+// {
+//     echo "Conexao efetuada com sucesso";
+// }
+
+
+
+?>
